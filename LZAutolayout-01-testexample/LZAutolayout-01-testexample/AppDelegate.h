36607121e7c9096af6910b14b9eb8f421979cf8b
LiuZHao <comst@comstdeMacBook-Pro.local>
2015-10-05 11:06:33 +0800

    init

---
+++ LZAutolayout-01-testexample/LZAutolayout-01-testexample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LZAutolayout-01-testexample
+//
+//  Created by comst on 15/10/5.
+//  Copyright (c) 2015年 com.comst1314. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
